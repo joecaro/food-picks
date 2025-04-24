9b818102b5d752da5f6761358d9a7746a09fd668
Joseph Carothers <joe@trysalient.com>
2025-04-23 18:01:15 -0700

    Update TournamentDetailPage to include optional searchParams in props

--- app/tournaments/[id]/page.tsx
+++ app/tournaments/[id]/page.tsx
@@ -10,7 +10,7 @@ import TournamentBracketVisualization from '../../components/TournamentBracketVi
 import { useAuth } from '../../auth-provider';
 import { useTournament, useStartVoting } from '../../../lib/hooks/useTournaments';
 
-export default function TournamentDetailPage({ params }: { params: { id: string } }) {
+export default function TournamentDetailPage({ params }: { params: { id: string }; searchParams?: { [key: string]: string | string[] | undefined } }) {
   const unwrappedParams = React.use(params as unknown as Promise<{ id: string }>);
   const tournamentId = unwrappedParams.id;
   const { user, isLoading: authLoading } = useAuth();
